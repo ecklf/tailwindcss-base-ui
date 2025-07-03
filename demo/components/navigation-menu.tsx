01d6a27106f4b2e9cdefd7f767dd49580fbce84c
Florentin Eckl <ecklf@icloud.com>
2025-07-03 16:17:34 +0200

    port

--- demo/components/navigation-menu.tsx
+++ demo/components/navigation-menu.tsx
@@ -22,11 +22,11 @@ export function NavigationMenuDemo() {
             className={clsx(
               "w-auto h-full",
               "transition-[opacity,transform,translate] duration-[var(--duration)] ease-[var(--easing)] ",
-              "data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 ",
-              "data-[starting-style]:data-[activation-direction=left]:translate-x-[-50%] ",
-              "data-[starting-style]:data-[activation-direction=right]:translate-x-[50%] ",
-              "data-[ending-style]:data-[activation-direction=left]:translate-x-[50%] ",
-              "data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]"
+              "bui-starting-style:opacity-0 bui-ending-style:opacity-0 ",
+              "bui-starting-style:bui-activation-direction-left:translate-x-[-50%] ",
+              "bui-starting-style:bui-activation-direction-right:translate-x-[50%] ",
+              "bui-ending-style:bui-activation-direction-left:translate-x-[50%] ",
+              "bui-ending-style:bui-activation-direction-right:translate-x-[-50%]"
             )}
           >
             <div className="w-[21rem] lg:w-[23rem] p-3 bg-whtie dark:bg-gray-800">
@@ -58,11 +58,11 @@ export function NavigationMenuDemo() {
             className={clsx(
               "w-auto h-full",
               "transition-[opacity,transform,translate] duration-[var(--duration)] ease-[var(--easing)] ",
-              "data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 ",
-              "data-[starting-style]:data-[activation-direction=left]:translate-x-[-50%] ",
-              "data-[starting-style]:data-[activation-direction=right]:translate-x-[50%] ",
-              "data-[ending-style]:data-[activation-direction=left]:translate-x-[50%] ",
-              "data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]"
+              "bui-starting-style:opacity-0 bui-ending-style:opacity-0 ",
+              "bui-starting-style:bui-activation-direction-left:translate-x-[-50%] ",
+              "bui-starting-style:bui-activation-direction-right:translate-x-[50%] ",
+              "bui-ending-style:bui-activation-direction-left:translate-x-[50%] ",
+              "bui-ending-style:bui-activation-direction-right:translate-x-[-50%]"
             )}
           >
             <div className="w-[16rem] lg:w-[18rem] p-3 bg-white dark:bg-gray-800">
@@ -89,7 +89,7 @@ export function NavigationMenuDemo() {
                     "px-4 py-3 w-full rounded-md hover:bg-gray-100 dark:hover:bg-gray-900",
                     "focus:outline-hidden focus-visible:ring-3 focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
                   )}
-                  href="https://www.radix-ui.com"
+                  href="https://base-ui.com"
                 >
                   <span className="text-sm font-medium text-gray-900 dark:text-gray-100">
                     Base UI
@@ -123,7 +123,7 @@ export function NavigationMenuDemo() {
           sideOffset={16}
           collisionPadding={{ top: 5, bottom: 5, left: 20, right: 20 }}
           className={clsx(
-            "box-border h-[var(--positioner-height)] w-[var(--positioner-width)] max-w-[var(--available-width)] transition-[top,left,right,bottom] duration-[var(--duration)] ease-[var(--easing)] before:absolute before:content-[''] data-[instant]:transition-none data-[side=bottom]:before:top-[-10px] data-[side=bottom]:before:right-0 data-[side=bottom]:before:left-0 data-[side=bottom]:before:h-2.5 data-[side=left]:before:top-0 data-[side=left]:before:right-[-10px] data-[side=left]:before:bottom-0 data-[side=left]:before:w-2.5 data-[side=right]:before:top-0 data-[side=right]:before:bottom-0 data-[side=right]:before:left-[-10px] data-[side=right]:before:w-2.5 data-[side=top]:before:right-0 data-[side=top]:before:bottom-[-10px] data-[side=top]:before:left-0 data-[side=top]:before:h-2.5"
+            "box-border h-bui-positioner w-bui-positioner max-w-[var(--available-width)] transition-[top,left,right,bottom] duration-[var(--duration)] ease-[var(--easing)] before:absolute before:content-[''] data-[instant]:transition-none bui-side-bottom:before:top-[-10px] bui-side-bottom:before:right-0 bui-side-bottom:before:left-0 bui-side-bottom:before:h-2.5 bui-side-left:before:top-0 bui-side-left:before:right-[-10px] bui-side-left:before:bottom-0 bui-side-left:before:w-2.5 bui-side-right:before:top-0 bui-side-right:before:bottom-0 bui-side-right:before:left-[-10px] bui-side-right:before:w-2.5 bui-side-top:before:right-0 bui-side-top:before:bottom-[-10px] bui-side-top:before:left-0 bui-side-top:before:h-2.5"
           )}
           style={{
             ["--duration" as string]: "0.35s",
@@ -132,19 +132,18 @@ export function NavigationMenuDemo() {
         >
           <NavigationMenu.Popup
             className={clsx(
-              "shadow-lg rounded-md bg-white dark:bg-gray-800 overflow-hidden",
-              "data-[ending-style]:easing-[ease] relative h-[var(--popup-height)] w-max origin-[var(--transform-origin)]",
-              "transition-[opacity,transform,width,height,scale,translate] duration-[var(--duration)] ease-[var(--easing)] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[ending-style]:duration-150 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 min-[500px]:w-[var(--popup-width)] xs:w-[var(--popup-width)] dark:outline-gray-300"
+              "bui-ending-style:easing-[ease] relative h-bui-popup w-max bui-origin",
+              "transition-[opacity,transform,width,height,scale,translate] duration-[var(--duration)] ease-[var(--easing)] bui-ending-style:scale-90 bui-ending-style:opacity-0 bui-ending-style:duration-150 bui-starting-style:scale-90 bui-starting-style:opacity-0 min-[500px]:w-[var(--popup-width)] xs:w-[var(--popup-width)] dark:outline-gray-300"
             )}
           >
             <NavigationMenu.Arrow
               className={clsx(
-                "flex transition-[left] duration-[var(--duration)] ease-[var(--easing)] data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180"
+                "flex transition-[left] duration-[var(--duration)] ease-[var(--easing)] bui-side-bottom:top-[-8px] bui-side-left:right-[-13px] bui-side-left:rotate-90 bui-side-right:left-[-13px] bui-side-right:-rotate-90 bui-side-top:bottom-[-8px] bui-side-top:rotate-180"
               )}
             >
               <div className="top-1 relative bg-white dark:bg-gray-800 w-2 h-2 rotate-45" />
             </NavigationMenu.Arrow>
-            <NavigationMenu.Viewport className="relative h-full w-full" />
+            <NavigationMenu.Viewport className="relative h-full w-full shadow-lg rounded-md bg-white dark:bg-gray-800 overflow-hidden" />
           </NavigationMenu.Popup>
         </NavigationMenu.Positioner>
       </NavigationMenu.Portal>
