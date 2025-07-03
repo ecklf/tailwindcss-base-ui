50eaa769914f27e2f1a299e8e0d9bbe7befa2c6a
Florentin Eckl <ecklf@icloud.com>
2025-07-03 16:08:25 +0200

    fix port

--- demo/components/navigation-menu.tsx
+++ demo/components/navigation-menu.tsx
@@ -20,7 +20,7 @@ export function NavigationMenuDemo() {
 
           <NavigationMenu.Content
             className={clsx(
-              "w-auto h-full rounded-lg bg-white dark:bg-gray-800",
+              "w-auto h-full",
               "transition-[opacity,transform,translate] duration-[var(--duration)] ease-[var(--easing)] ",
               "data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 ",
               "data-[starting-style]:data-[activation-direction=left]:translate-x-[-50%] ",
@@ -29,7 +29,7 @@ export function NavigationMenuDemo() {
               "data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]"
             )}
           >
-            <div className="w-[21rem] lg:w-[23rem] p-3">
+            <div className="w-[21rem] lg:w-[23rem] p-3 bg-whtie dark:bg-gray-800">
               <div className="grid grid-cols-6 gap-4">
                 <div className="col-span-2 p-4 w-full bg-gray-100 rounded-md dark:bg-gray-900" />
                 <div className="flex flex-col col-span-4 p-4 space-y-3 w-full bg-gray-100 rounded-md dark:bg-gray-900">
@@ -56,7 +56,7 @@ export function NavigationMenuDemo() {
 
           <NavigationMenu.Content
             className={clsx(
-              "w-auto h-full rounded-lg bg-white dark:bg-gray-800",
+              "w-auto h-full",
               "transition-[opacity,transform,translate] duration-[var(--duration)] ease-[var(--easing)] ",
               "data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 ",
               "data-[starting-style]:data-[activation-direction=left]:translate-x-[-50%] ",
@@ -65,7 +65,7 @@ export function NavigationMenuDemo() {
               "data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]"
             )}
           >
-            <div className="w-[16rem] lg:w-[18rem] p-3">
+            <div className="w-[16rem] lg:w-[18rem] p-3 bg-white dark:bg-gray-800">
               <div className="flex flex-col space-y-2 w-full">
                 <NavigationMenu.Link
                   className={clsx(
@@ -122,13 +122,21 @@ export function NavigationMenuDemo() {
         <NavigationMenu.Positioner
           sideOffset={16}
           collisionPadding={{ top: 5, bottom: 5, left: 20, right: 20 }}
-          className="box-border h-[var(--positioner-height)] w-[var(--positioner-width)] max-w-[var(--available-width)] transition-[top,left,right,bottom] duration-[var(--duration)] ease-[var(--easing)] before:absolute before:content-[''] data-[instant]:transition-none data-[side=bottom]:before:top-[-10px] data-[side=bottom]:before:right-0 data-[side=bottom]:before:left-0 data-[side=bottom]:before:h-2.5 data-[side=left]:before:top-0 data-[side=left]:before:right-[-10px] data-[side=left]:before:bottom-0 data-[side=left]:before:w-2.5 data-[side=right]:before:top-0 data-[side=right]:before:bottom-0 data-[side=right]:before:left-[-10px] data-[side=right]:before:w-2.5 data-[side=top]:before:right-0 data-[side=top]:before:bottom-[-10px] data-[side=top]:before:left-0 data-[side=top]:before:h-2.5"
+          className={clsx(
+            "box-border h-[var(--positioner-height)] w-[var(--positioner-width)] max-w-[var(--available-width)] transition-[top,left,right,bottom] duration-[var(--duration)] ease-[var(--easing)] before:absolute before:content-[''] data-[instant]:transition-none data-[side=bottom]:before:top-[-10px] data-[side=bottom]:before:right-0 data-[side=bottom]:before:left-0 data-[side=bottom]:before:h-2.5 data-[side=left]:before:top-0 data-[side=left]:before:right-[-10px] data-[side=left]:before:bottom-0 data-[side=left]:before:w-2.5 data-[side=right]:before:top-0 data-[side=right]:before:bottom-0 data-[side=right]:before:left-[-10px] data-[side=right]:before:w-2.5 data-[side=top]:before:right-0 data-[side=top]:before:bottom-[-10px] data-[side=top]:before:left-0 data-[side=top]:before:h-2.5"
+          )}
           style={{
             ["--duration" as string]: "0.35s",
             ["--easing" as string]: "cubic-bezier(0.22, 1, 0.36, 1)",
           }}
         >
-          <NavigationMenu.Popup className="data-[ending-style]:easing-[ease] relative h-[var(--popup-height)] w-max origin-[var(--transform-origin)] rounded-lg bg-[canvas] text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[opacity,transform,width,height,scale,translate] duration-[var(--duration)] ease-[var(--easing)] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[ending-style]:duration-150 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 min-[500px]:w-[var(--popup-width)] xs:w-[var(--popup-width)] dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300">
+          <NavigationMenu.Popup
+            className={clsx(
+              "shadow-lg rounded-md bg-white dark:bg-gray-800 overflow-hidden",
+              "data-[ending-style]:easing-[ease] relative h-[var(--popup-height)] w-max origin-[var(--transform-origin)]",
+              "transition-[opacity,transform,width,height,scale,translate] duration-[var(--duration)] ease-[var(--easing)] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[ending-style]:duration-150 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 min-[500px]:w-[var(--popup-width)] xs:w-[var(--popup-width)] dark:outline-gray-300"
+            )}
+          >
             <NavigationMenu.Arrow
               className={clsx(
                 "flex transition-[left] duration-[var(--duration)] ease-[var(--easing)] data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180"
@@ -136,7 +144,7 @@ export function NavigationMenuDemo() {
             >
               <div className="top-1 relative bg-white dark:bg-gray-800 w-2 h-2 rotate-45" />
             </NavigationMenu.Arrow>
-            <NavigationMenu.Viewport className="relative h-full w-full overflow-hidden" />
+            <NavigationMenu.Viewport className="relative h-full w-full" />
           </NavigationMenu.Popup>
         </NavigationMenu.Positioner>
       </NavigationMenu.Portal>
