52be4572ad173ab9a375fb50caba2156f2936b7a
Florentin Eckl <ecklf@icloud.com>
2025-07-03 16:01:47 +0200

    fix menu

--- demo/components/navigation-menu.tsx
+++ demo/components/navigation-menu.tsx
@@ -1,8 +1,8 @@
+import type React from "react";
 import { NavigationMenu } from "@base-ui-components/react/navigation-menu";
 import { clsx } from "clsx";
-import React from "react";
 
-export const NavigationMenuDemo = () => {
+export function NavigationMenuDemo() {
   return (
     <NavigationMenu.Root className="relative">
       <NavigationMenu.List className="flex flex-row p-2 space-x-2 bg-white rounded-lg dark:bg-gray-800">
@@ -20,17 +20,18 @@ export const NavigationMenuDemo = () => {
 
           <NavigationMenu.Content
             className={clsx(
-              "absolute top-0 left-0 w-auto rounded-lg",
-              "radix-motion-from-start:animate-enter-from-left",
-              "radix-motion-from-end:animate-enter-from-right",
-              "radix-motion-to-start:animate-exit-to-left",
-              "radix-motion-to-end:animate-exit-to-right"
+              "w-auto h-full rounded-lg bg-white dark:bg-gray-800",
+              "transition-[opacity,transform,translate] duration-[var(--duration)] ease-[var(--easing)] ",
+              "data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 ",
+              "data-[starting-style]:data-[activation-direction=left]:translate-x-[-50%] ",
+              "data-[starting-style]:data-[activation-direction=right]:translate-x-[50%] ",
+              "data-[ending-style]:data-[activation-direction=left]:translate-x-[50%] ",
+              "data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]"
             )}
           >
             <div className="w-[21rem] lg:w-[23rem] p-3">
               <div className="grid grid-cols-6 gap-4">
                 <div className="col-span-2 p-4 w-full bg-gray-100 rounded-md dark:bg-gray-900" />
-
                 <div className="flex flex-col col-span-4 p-4 space-y-3 w-full bg-gray-100 rounded-md dark:bg-gray-900">
                   <div className="w-full h-12 bg-white rounded-md dark:bg-gray-700" />
                   <div className="w-full h-12 bg-white rounded-md dark:bg-gray-700" />
@@ -55,11 +56,13 @@ export const NavigationMenuDemo = () => {
 
           <NavigationMenu.Content
             className={clsx(
-              "absolute top-0 left-0 w-auto rounded-lg",
-              "radix-motion-from-start:animate-enter-from-left",
-              "radix-motion-from-end:animate-enter-from-right",
-              "radix-motion-to-start:animate-exit-to-left",
-              "radix-motion-to-end:animate-exit-to-right"
+              "w-auto h-full rounded-lg bg-white dark:bg-gray-800",
+              "transition-[opacity,transform,translate] duration-[var(--duration)] ease-[var(--easing)] ",
+              "data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 ",
+              "data-[starting-style]:data-[activation-direction=left]:translate-x-[-50%] ",
+              "data-[starting-style]:data-[activation-direction=right]:translate-x-[50%] ",
+              "data-[ending-style]:data-[activation-direction=left]:translate-x-[50%] ",
+              "data-[ending-style]:data-[activation-direction=right]:translate-x-[-50%]"
             )}
           >
             <div className="w-[16rem] lg:w-[18rem] p-3">
@@ -102,9 +105,9 @@ export const NavigationMenuDemo = () => {
           </NavigationMenu.Content>
         </NavigationMenu.Item>
 
-        <NavigationMenu.Item>
+        <NavigationMenu.Item className="flex items-center">
           <NavigationMenu.Link
-            href="https://github.com/ecklf/tailwindcss-radix"
+            href="https://github.com/ecklf/tailwindcss-base-ui"
             className={clsx(
               "px-3 py-2 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900",
               "text-sm font-medium text-gray-700 dark:text-gray-100"
@@ -117,29 +120,26 @@ export const NavigationMenuDemo = () => {
 
       <NavigationMenu.Portal>
         <NavigationMenu.Positioner
-          className={clsx(
-            "absolute flex justify-center",
-            "w-[140%] left-[-20%] top-[100%]"
-          )}
+          sideOffset={16}
+          collisionPadding={{ top: 5, bottom: 5, left: 20, right: 20 }}
+          className="box-border h-[var(--positioner-height)] w-[var(--positioner-width)] max-w-[var(--available-width)] transition-[top,left,right,bottom] duration-[var(--duration)] ease-[var(--easing)] before:absolute before:content-[''] data-[instant]:transition-none data-[side=bottom]:before:top-[-10px] data-[side=bottom]:before:right-0 data-[side=bottom]:before:left-0 data-[side=bottom]:before:h-2.5 data-[side=left]:before:top-0 data-[side=left]:before:right-[-10px] data-[side=left]:before:bottom-0 data-[side=left]:before:w-2.5 data-[side=right]:before:top-0 data-[side=right]:before:bottom-0 data-[side=right]:before:left-[-10px] data-[side=right]:before:w-2.5 data-[side=top]:before:right-0 data-[side=top]:before:bottom-[-10px] data-[side=top]:before:left-0 data-[side=top]:before:h-2.5"
           style={{
-            perspective: "2000px",
+            ["--duration" as string]: "0.35s",
+            ["--easing" as string]: "cubic-bezier(0.22, 1, 0.36, 1)",
           }}
         >
-          <NavigationMenu.Popup>
-            <NavigationMenu.Viewport
+          <NavigationMenu.Popup className="data-[ending-style]:easing-[ease] relative h-[var(--popup-height)] w-max origin-[var(--transform-origin)] rounded-lg bg-[canvas] text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[opacity,transform,width,height,scale,translate] duration-[var(--duration)] ease-[var(--easing)] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[ending-style]:duration-150 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 min-[500px]:w-[var(--popup-width)] xs:w-[var(--popup-width)] dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300">
+            <NavigationMenu.Arrow
               className={clsx(
-                "overflow-hidden relative mt-2 bg-white rounded-md shadow-lg dark:bg-gray-800",
-                "w-radix-navigation-menu-viewport",
-                "h-radix-navigation-menu-viewport",
-                "radix-state-open:animate-scale-in-content",
-                "radix-state-closed:animate-scale-out-content",
-                "duration-300 origin-[top_center] transition-[width_height] ease-[ease]"
+                "flex transition-[left] duration-[var(--duration)] ease-[var(--easing)] data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180"
               )}
-            />
-            <NavigationMenu.Arrow className="relative top-1 w-2 h-2 bg-white rotate-45 dark:bg-gray-800" />
+            >
+              <div className="top-1 relative bg-white dark:bg-gray-800 w-2 h-2 rotate-45" />
+            </NavigationMenu.Arrow>
+            <NavigationMenu.Viewport className="relative h-full w-full overflow-hidden" />
           </NavigationMenu.Popup>
         </NavigationMenu.Positioner>
       </NavigationMenu.Portal>
     </NavigationMenu.Root>
   );
-};
+}
